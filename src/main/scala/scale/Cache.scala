edd59b9b362fadca4d71df7f342b2b3411177518
lfd <617390727@qq.com>
2018-03-27 09:54:25 +0800

    update cache.

--- src/main/scala/scale/Cache.scala
+++ src/main/scala/scale/Cache.scala
@@ -4,44 +4,45 @@ import chisel3._
 import chisel3.util._
 
 
-abstract trait CoreParams {
-  val addrLength = 32
+trait CoreParams {
+  val addrWidth = 32
 }
 
-class CacheRequest extends Bundle with CoreParams{
-  val addr = UInt(addrLength.W)
+class CacheRequest extends Bundle with CoreParams {
+  val addr = UInt(addrWidth.W)
 }
 
-class CacheRespond extends Bundle with CoreParams{
-  val data = UInt(addrLength.W)
+class CacheResponse extends Bundle with CacheParams {
+  val data = UInt(blockSize.W)
 }
 
-class CacheIO extends Bundle{
-  val req   = Flipped(Valid(new CacheRequest))
-  val resp  = Valid(new CacheRespond)
+class CacheIO extends Bundle {
+  val req = Flipped(Valid(new CacheRequest))
+  val resp = Valid(new CacheResponse)
 }
 
-trait CacheParams extends CoreParams{
-  val cacheCap = 1<<20 //1MB
-  val blockSize = 1<<9 //64Byte
+trait CacheParams extends CoreParams {
+  val capacity = 1 << 20 //1MB
+  val blockSize = 1 << 9 //64Byte
   val assoc = 8
-  val numSets = cacheCap / blockSize / assoc
+  val numSets = capacity / blockSize / assoc
 
-  val blockoffsetLength = log2Up(blockSize)
-  val setLength = log2Up(numSets)
-  val tagLength = addrLength - (blockoffsetLength + setLength)
+  val blockOffsetWidth = log2Up(blockSize)
+  val setIndexWidth = log2Up(numSets)
+  val tagWidth = addrWidth - blockOffsetWidth - setIndexWidth
 }
 
-class IndexItem extends Bundle with CacheParams{
-  val v = Bool()
-  val tag = UInt(tagLength.W)
+class CacheBlock extends Bundle with CacheParams {
+  val valid = Bool()
+  val tag = UInt(tagWidth.W)
+  val data = UInt(blockSize.W)
 }
 
 class Cache extends Module with CacheParams {
   val io = IO(new CacheIO)
-  val indexTable = Mem(numSets,Vec(assoc,new IndexItem))
-  val cacheMem = Vec(cacheCap / blockSize,UInt(blockSize.W))
 
+  val blocks = Vec(numSets, Vec(assoc, new CacheBlock))
 
+  //  def compare
 
 }
