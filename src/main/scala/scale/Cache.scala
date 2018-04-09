219314bd3fb2ba2afc5522d2e5ddf27e9f775634
lfd <617390727@qq.com>
2018-04-10 02:16:12 +0800

    update LFU.

--- src/main/scala/scale/Cache.scala
+++ src/main/scala/scale/Cache.scala
@@ -8,9 +8,9 @@ class Cache extends Module with Params {
 
   val ren = io.req.valid
   val ren_reg = RegNext(ren)
-  val blocks = Vec(numSets, Vec(assoc, new CacheBlock))
+  val blocks = Mem(numSets, VecInit(Seq.fill(assoc)(new CacheBlock)))
 
-  val counter_LFU = Vec(numSets, VecInit(Seq.fill(assoc)(0.U(4.W))))
+  val counter_LFU = Mem(numSets, VecInit(Seq.fill(assoc)(0.U(4.W))))
 
   io.resp.valid := false.B
   io.resp.bits := DontCare
@@ -21,16 +21,15 @@ class Cache extends Module with Params {
   val sets = blocks(setIndex)
 
   val hit: Bool = sets.exists((block: CacheBlock) => block.valid && block.tag === tag)
-
   // read
   when(hit && ren_reg) {
     val blockFoundId: UInt = sets.indexWhere((block: CacheBlock) => block.valid && block.tag === tag)
     val blockFound = sets(blockFoundId)
 
     for (n <- 0 until assoc - 1) {
-      counter_LFU(setIndex)(n) = counter_LFU(setIndex)(n) + 1.U
+      counter_LFU(setIndex)(n) := counter_LFU(setIndex)(n) + 1.U   //fixthis
     }
-    counter_LFU(setIndex)(blockFoundId) = 0.U
+    counter_LFU(setIndex)(blockFoundId) := 0.U
 
     io.resp.valid := true.B
     io.resp.bits.data := blockFound.data
@@ -44,14 +43,14 @@ class Cache extends Module with Params {
         topCountID := n.asUInt()
         tempCount := counter_LFU(setIndex)(n)
       }
-      counter_LFU(setIndex)(n) = counter_LFU(setIndex)(n) + 1.U
+      counter_LFU(setIndex)(n) := counter_LFU(setIndex)(n) + 1.U
     }
     //todo:send read request to mem
 
     val memData = Reg(io.resp.bits.data.cloneType)
-    blocks(setIndex)(topCountID) = memData
+    blocks(setIndex)(topCountID) := memData
 
-    counter_LFU(setIndex)(topCountID) = 0.U
+    counter_LFU(setIndex)(topCountID) := 0.U
   }
 
 
